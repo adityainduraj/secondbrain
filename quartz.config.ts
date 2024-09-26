b8305b612c03357e3814757a193ba69655cd4078
adityainduraj <85331139+adityainduraj@users.noreply.github.com>
2024-09-26 10:05:44 +0530

    Quartz sync: Sep 26, 2024, 10:05 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,6 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "🧠 Second Brain",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
